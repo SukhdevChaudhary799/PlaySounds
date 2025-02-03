023c18c8b3ed8e82556ae23d07eddf1784501fd7
Sukhdev <sukhdev.chaudhary@addverb.com>
2025-02-03 10:12:12 +0530

    Added more sounds to the list

--- PlaySound.cpp
+++ PlaySound.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <vector>
 #include <chrono>
+#include <iostream>
 
 void playAudio(const std::string& filePath) {
     sf::SoundBuffer buffer;
@@ -17,10 +18,12 @@ int main() {
     std::vector<std::string> audioFiles = {"Sound1.wav", "Sound2.wav", "Sound3.wav","Sound4.wav", "Sound5.wav", "Sound6.wav", "Sound7.wav" ,"Sound8.wav","Sound1.wav", "Sound2.wav", "Sound3.wav","Sound4.wav", "Sound5.wav", "Sound6.wav", "Sound7.wav" ,"Sound8.wav"};
 
     for (const auto& file : audioFiles) {
+        std::cerr<<"Making thread"<<std::endl;
         std::thread(playAudio, file).detach();
-        std::this_thread::sleep_for(std::chrono::seconds(2));  // Pattern delay
+        std::cerr<<"Sleeping for 2 seconds"<<std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(4));  // Pattern delay
     }
-
-    std::this_thread::sleep_for(std::chrono::seconds(10));  // Prevent main from exiting early
+    std::cerr<<"after for loop sleeping for 10sec"<<std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));  // Prevent main from exiting early
     return 0;
 }
